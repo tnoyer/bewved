4b4a0bf4cebd0d8506b63403c0a01de130a2025f
Kemrone <mcroain@gmail.com>
2022-05-18 13:48:23 +0200

    pray commit

--- src/Controller/FrontOffice/HomeController.php
+++ src/Controller/FrontOffice/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('front/home/index.html.twig', [
+        return $this->render('/front/home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
